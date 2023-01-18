cf3eccf233f0e6605190ded6b3363f0d3674948a
ofer <ofergavri@gmail.com>
2023-01-18 14:30:57 +0200

    style board header

--- sprint-4/frontend/src/cmps/board/board-header.jsx
+++ sprint-4/frontend/src/cmps/board/board-header.jsx
@@ -9,20 +9,28 @@ export function BoardHeader() {
     return (
         <header className="board-header">
 
-            <div className="board-description">
+            <div className="board-info">
                 <h1>Sprint 4</h1>
                 <div><RiErrorWarningLine className='icon' /></div>
                 <div><BsStar className='icon' /></div>
-                <div><FiActivity className='icon' /></div>
+                <div className='board-description'>
+                    <h5 className='board-description-link'>Add your board's description here <span>See More</span></h5>
+                </div>
             </div>
 
             <div className='board-tools flex'>
-                <div className='members-last-seen'>
-                    <span>Last seen</span>
-                    <div className='members-imgs-container flex'>
+                <div className='activity'><FiActivity /></div>
 
-                        <img className='member-img' src={guest} alt="" />
-                        <img className='member-img' src={guest} alt="" />
+                <div className='members-last-seen'>
+                    <span className='last-seen-title'>Last seen</span>
+                    <div className='flex members-imgs'>
+
+                        <img className='member-img1' src={guest} alt="" />
+                        <img className='member-img2' src={guest} alt="" />
+                        {/* if there more than 2 members */}
+                        <div className='show-more-members'>
+                            <span className='show-more-count'>+2</span>
+                        </div>
                     </div>
                 </div>
             </div>
