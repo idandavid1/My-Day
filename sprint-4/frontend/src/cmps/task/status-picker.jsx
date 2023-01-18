efdea4c567971c7b25743dff4f2ae39c813a389c
Idan David <idand2000@gmail.com>
2023-01-18 14:48:38 +0200

    status picker

--- sprint-4/frontend/src/cmps/task/status-picker.jsx
+++ sprint-4/frontend/src/cmps/task/status-picker.jsx
@@ -1,6 +1,7 @@
 
 
 export function StatusPicker({ info, onUpdate}) {
+    boards = use
     const label = info.labels.find(label => label.title === info.status)
     const color = label.color ? label.color : 'blue'
     return <section className="status-picker" style={{backgroundColor: color}}>
