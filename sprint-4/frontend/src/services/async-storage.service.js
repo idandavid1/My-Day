0f4b646018c151890341e9559fb6f76835e23327
Idan David <idand2000@gmail.com>
2023-01-19 15:28:49 +0200

    update scss

--- sprint-4/frontend/src/services/async-storage.service.js
+++ sprint-4/frontend/src/services/async-storage.service.js
@@ -6,7 +6,7 @@ export const storageService = {
     remove,
 }
 
-function query(entityType, delay = 500) {
+function query(entityType, delay = 100) {
     var entities = JSON.parse(localStorage.getItem(entityType)) || []
     return new Promise(resolve => setTimeout(() => resolve(entities), delay))
 }
