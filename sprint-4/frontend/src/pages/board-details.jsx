154a2c0f0599625473fd344d047f0670012854c0
Idan David <idand2000@gmail.com>
2023-01-19 14:02:10 +0200

    fix bug

--- sprint-4/frontend/src/pages/board-details.jsx
+++ sprint-4/frontend/src/pages/board-details.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-import { loadBoard } from "../store/board.actions"
+import { loadBoard, loadBoards } from "../store/board.actions"
 
 import { BoardHeader } from "../cmps/board/board-header"
 import { MainSidebar } from "../cmps/sidebar/main-sidebar"
@@ -11,10 +11,12 @@ import { useParams } from 'react-router-dom'
 
 export function BoardDetails() {
     const board = useSelector(storeState => storeState.boardModule.board)
+    const boards = useSelector(storeState => storeState.boardModule.boards)
     const { boardId } = useParams()
 
     useEffect(() => {
         loadBoard(boardId)
+        if(!boards.length) loadBoards()
     }, [])
 
     if(!board) return <div>Loading...</div>
