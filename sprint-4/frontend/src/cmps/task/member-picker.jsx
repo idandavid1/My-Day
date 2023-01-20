ae5e402fc7a59ef4cb83000e4d3643c2c5ba7151
Idan David <idand2000@gmail.com>
2023-01-20 16:45:15 +0200

    add modal member

--- sprint-4/frontend/src/cmps/task/member-picker.jsx
+++ sprint-4/frontend/src/cmps/task/member-picker.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 import { useSelector } from "react-redux"
+import { ModalMember } from "../modal-member"
 
 const guest = require('../../assets/img/guest.png')
 
 export function MemberPicker({ info, onUpdate }) {
     const board = useSelector(storeState => storeState.boardModule.board)
-    if (!info.memberIds) return <div className="picker task-person"></div>
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    if (!info.memberIds) return <div onClick={() => setIsModalOpen(true)} style={{width: '87px'}} className="picker task-person"></div>
     const members = info.memberIds.map(member => getMember(member))
     function getMember(memberId) {
         return board.members.find(member => member.id === memberId)
@@ -12,11 +16,13 @@ export function MemberPicker({ info, onUpdate }) {
 
     return (
         <section className="flex members-imgs justify-center picker task-person">
-            {members.length > 0 && <img className='member-img1' src={!members.length ? guest : members[0].imgUrl} alt="member" />}
-            {members.length === 2 && <img className='member-img2' src={members.length <= 1 ? guest : members[1].imgUrl} alt="member" />}
-            {members.length > 1 && <div className='show-more-members'>
+            {members.length === 0 && <img className='member-img1' src={guest} alt="member" onClick={() => setIsModalOpen(true)} />}
+            {members.length > 0 && <img className='member-img1' src={!members.length ? guest : members[0].imgUrl} alt="member" onClick={() => setIsModalOpen(true)} />}
+            {members.length === 2 && <img className='member-img2' src={members.length <= 1 ? guest : members[1].imgUrl} alt="member" onClick={() => setIsModalOpen(true)}/>}
+            {members.length > 1 && <div className='show-more-members' onClick={() => setIsModalOpen(true)}>
                 <span className='show-more-count'>+{members.length - 1}</span>
             </div>}
+            {isModalOpen && <ModalMember taskMembers={members} onUpdate={onUpdate} setIsModalOpen={setIsModalOpen} cmpType={'memberIds'}/>}
         </section>
     )
 }
