f1863d329321fa696b1ab1aac0f51b1bd8891fb8
Idan David <idand2000@gmail.com>
2023-01-20 11:09:56 +0200

    update status modal

--- sprint-4/frontend/src/cmps/task/priority-picker.jsx
+++ sprint-4/frontend/src/cmps/task/priority-picker.jsx
@@ -5,18 +5,18 @@ import { ModalStatusPriority } from "../modal-status-priority"
 export function PriorityPicker({ info, onUpdate }) {
     const board = useSelector(storeState => storeState.boardModule.board)
     const [isModalOpen, setIsModalOpen] = useState(false)
-    const [position, setPosition] = useState({ x: 0, y: 0 })
 
     function onClick(ev) {
-        setPosition({x:ev.pageX, y:ev.pageY})
         setIsModalOpen(true)
     }
 
+    let classText = !info.priority ? 'picker empty-label' : ''
+    classText += isModalOpen ? ' modal-open' : ''
     const label = board.labels.find(label => label.title === info.priority)
     const color = label ? label.color : '#c4c4c4'
     
     return <section className="priority-picker picker label" style={{ backgroundColor: color }}>
-        <div className={!info.priority ? 'picker empty-label' : ''} onClick={onClick}>{info.priority}</div>
-        {isModalOpen && <ModalStatusPriority labels={board.labels} onUpdate={onUpdate} position={position} setIsModalOpen={setIsModalOpen} cmpType={'priority'}/>}
+        <div className={classText} onClick={onClick}>{info.priority}</div>
+        {isModalOpen && <ModalStatusPriority labels={board.labels} onUpdate={onUpdate} setIsModalOpen={setIsModalOpen} cmpType={'priority'}/>}
     </section>
 }
