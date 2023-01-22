0e8a600e53de1023e904dfc4579e98a0fcf5fe0a
ofer <ofergavri@gmail.com>
2023-01-22 10:45:46 +0200

    fix bugs

--- sprint-4/frontend/src/cmps/board/group-list.jsx
+++ sprint-4/frontend/src/cmps/board/group-list.jsx
@@ -3,27 +3,33 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 export function GroupList({ board }) {
     // return (
-    //     <DragDropContext onDragEnd={(result) => console.log('result', result)}>
-    //         {(provided, snapchat) => (
-    //             <div ref={provided.innerRef}
-    //                 {...provided.draggableProps}
-    //                 {...provided.dragHandleProps}>
+    //     <>
+    //         <DragDropContext onDragEnd={(result) => console.log('result', result)}>
+    //             {(provided, snapchat) => (
+    //                 <div ref={provided.innerRef}
+    //                     {...provided.draggableProps}
+    //                     {...provided.dragHandleProps}>
 
-    //                 <section className="group-list">
-    //                     <ul>
-    //                         <Droppable >
-    //                             {board.groups.map((group, idx) => {
-    //                                 return (
-    //                                     <li key={idx}><GroupPreview group={group} board={board} provided={provided} snapchat={snapchat} /></li>
-    //                                 )
-    //                             })}
-    //                         </Droppable>
+    //                     <section className="group-list">
+    //                         <ul>
+    //                             {board.groups.map((group) =>
+    //                             (
+    //                                 <Droppable key={board._id} droppableId={board._id}>
+    //                                     <li key={group.id}>
+    //                                         <GroupPreview group={group} board={board} provided={provided} snapchat={snapchat} />
+    //                                     </li>
+    //                                 </Droppable>
+    //                             )
+    //                             )}
 
-    //                     </ul>
-    //                 </section>
-    //             </div>
-    //         )}
-    //     </DragDropContext>
+    //                         </ul>
+    //                     </section>
+    //                     {provided.placeHolder}
+    //                 </div>
+    //             )}
+    //         </DragDropContext>
+    //     </>
+    // )
     return (
 
         <section className="group-list">
