cef8c1a827e2997c850e142b1e0b3598166a8271
Ofek Abramovitch <ofek301156@gmail.com>
2023-01-23 19:53:01 +0200

    login page styling

--- sprint-4/frontend/src/cmps/login-page-header.jsx
+++ sprint-4/frontend/src/cmps/login-page-header.jsx
@@ -3,7 +3,7 @@ const logo = require('../assets/img/monday-logo.webp')
 export function LoginPageHeader() {
     return (
         <header className="login-page-header">
-            <img src={logo} alt="Logo" />
+            <img className="logo" src={logo} alt="Logo" />
         </header>
     )
 }
