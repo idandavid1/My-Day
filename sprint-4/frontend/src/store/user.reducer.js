ba88749ec7eeffc1f51ca90dd4373816da2206e7
Idan David <idand2000@gmail.com>
2023-02-15 14:38:49 +0200

    add user test

--- sprint-4/frontend/src/store/user.reducer.js
+++ sprint-4/frontend/src/store/user.reducer.js
@@ -1,31 +1,18 @@
 import { userService } from '../services/user.service.js'
 
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
-export const CHANGE_COUNT = 'CHANGE_COUNT'
 export const SET_USER = 'SET_USER'
 export const SET_WATCHED_USER = 'SET_WATCHED_USER'
 export const REMOVE_USER = 'REMOVE_USER'
 export const SET_USERS = 'SET_USERS'
-export const SET_SCORE = 'SET_SCORE'
 
 const initialState = {
     user: userService.getLoggedinUser(),
     users: []
 }
 
-export function userReducer(state = initialState, action) {
+export function userReducer(state = initialState, action = {}) {
     var newState = state
     switch (action.type) {
-        case INCREMENT:
-            newState = { ...state, count: state.count + 1 }
-            break
-        case DECREMENT:
-            newState = { ...state, count: state.count - 1 }
-            break
-        case CHANGE_COUNT:
-            newState = { ...state, count: state.count + action.diff }
-            break
         case SET_USER:
             newState = { ...state, user: action.user }
             break
@@ -41,14 +28,8 @@ export function userReducer(state = initialState, action) {
         case SET_USERS:
             newState = { ...state, users: action.users }
             break
-        case SET_SCORE:
-            newState = { ...state, user: { ...state.user, score: action.score } }
-            break
-        default:
+        default: return state
     }
-    // For debug:
-    // window.userState = newState
-    // console.log('State:', newState)
-    return newState
 
+    return newState
 }
