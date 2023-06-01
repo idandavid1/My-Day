a1604ec2125b7d2427cb469ab31604b1d9ac850d
Idan David <idand2000@gmail.com>
2023-06-01 14:24:58 +0300

    add board modal to dynamic modal

--- frontend/src/cmps/board/board-filter.jsx
+++ frontend/src/cmps/board/board-filter.jsx
@@ -49,7 +49,6 @@ export function BoardFilter({ board, onSetFilter }) {
     function onToggleMemberFilterModal() {
         const isOpen = dynamicModalObj?.type === 'member-filter' ? !dynamicModalObj.isOpen : true
         const { x, y } = elMemberFilter.current.getClientRects()[0]
-        console.log('11111:')
         setDynamicModalObj({ isOpen, pos: { x: (x - 160), y: (y + 40) }, type: 'member-filter', filterBy: filterBy, setFilterBy: setFilterBy})
     }
 
