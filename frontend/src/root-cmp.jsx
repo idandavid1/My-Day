b2ec91c720436a16f6aa61f31de30e3da36294db
ofer <ofergavri@gmail.com>
2023-06-01 15:38:59 +0300

    added titles and aria labels

--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 import { Provider } from 'react-redux'
 import { BoardDetails } from './pages/board-details'
-import { HomePage } from './pages/home-page'
+import  HomePage from './pages/home-page'
 import { LoginSignup } from './pages/login-signup'
 import { store } from './store/store'
 
-export function RootCmp() {
+export function RootCmp () {
     return (
         <Provider store={store}>
             <div>
@@ -14,10 +14,10 @@ export function RootCmp() {
                     <Routes>
                         <Route element={<HomePage />} path='/' />
                         <Route element={<BoardDetails />} path='/board/:boardId/' />
-                        <Route element={<BoardDetails />} path='/board/:boardId/:groupId/:taskId' />
-                        <Route element={<BoardDetails />} path='/board/:boardId/:activityLog' />
-                        <Route element={<LoginSignup />} path='/auth/login' />
-                        <Route element={<LoginSignup />} path='/auth/signup' />
+                                <Route element={<BoardDetails />} path='/board/:boardId/:groupId/:taskId' />
+                                <Route element={<BoardDetails />} path='/board/:boardId/:activityLog' />
+                                <Route element={<LoginSignup />} path='/auth/login' />
+                            <Route element={<LoginSignup />} path='/auth/signup' />
                     </Routes>
                 </main>
             </div>
