63be11dc5e9acd3d5afd52b4304b3f3f5ea30362
Idan David <idand2000@gmail.com>
2023-06-04 11:39:40 +0300

    clean code

--- backend/api/auth/auth.controller.js
+++ backend/api/auth/auth.controller.js
@@ -18,8 +18,6 @@ async function login(req, res) {
 async function signup(req, res) {
     try {
         const credentials = req.body
-        // Never log passwords
-        // logger.debug(credentials)
         const account = await authService.signup(credentials)
         logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
         const user = await authService.login(credentials.username, credentials.password)
--- backend/api/auth/auth.service.js
+++ backend/api/auth/auth.service.js
@@ -15,10 +15,6 @@ async function login(username, password) {
     logger.debug(`auth.service - login with username: ${username}`)
     const user = await userService.getByUsername(username)
     if (!user) return Promise.reject('Invalid username or password')
-    // TODO: un-comment for real login
-    // const match = await bcrypt.compare(password, user.password)
-    // if (!match) return Promise.reject('Invalid username or password')
-
     delete user.password
     user._id = user._id.toString()
     return user
@@ -55,11 +51,3 @@ function validateToken(loginToken) {
     }
     return null
 }
-
-
-
-
-// ;(async ()=>{
-//     await signup('bubu', '123', 'Bubu Bi')
-//     await signup('mumu', '123', 'Mumu Maha')
-// })()
